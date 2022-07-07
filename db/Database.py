4e170dad107df7c4937de861a755e6cb77c16c4e
IIDizzII <108428553+IIDizzII@users.noreply.github.com>
2022-07-07 13:09:07 +0300

    Add files via upload

---
+++ db/Database.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+def login(login, passw, signal):
+    con = sqlite3.connect('db/KMPdatabase.db')
+    cur = con.cursor()
+
+    cur.execute(f'SELECT * FROM users WHERE login="{login}";')
+    value = cur.fetchall()
+
+    if value != [] and value[0][2] == passw:
+        signal.emit('Успешная авторизация!')
+
+    else:
+        signal.emit('Проверьте правильность ввода данных!')
+
+    cur.close()
+    con.close()
+
+def register(login, passw, signal):
+    con = sqlite3.connect('db/KMPdatabase.db')
+    cur = con.cursor()
+
+    cur.execute(f'SELECT * FROM users WHERE login="{login}";')
+    value = cur.fetchall()
+
+    if value != []:
+        signal.emit('Такое имя уже используется!')
+    elif value == []:
+        cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{passw}')")
+        signal.emit('Вы успешно зарегистрированы!')
+        con.commit()
+
+    cur.close()
+    con.close()
